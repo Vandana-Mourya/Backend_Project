e63f2ad970dc30cea649416efd199dc955f135c2
Vandana Mourya <vandanamourya2002@gmail.com>
2024-08-22 16:10:44 +0530

    add new routes for user controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,19 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changePassword, 
+    getCurrentUser, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    registerUser, 
+    updateAvatar, 
+    updateCoverImage, 
+    updateUser
+ } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyToken } from "../middlewares/auth.middleware.js";
+import { verify } from "jsonwebtoken";
 
 
 const routes = Router()
@@ -24,5 +36,12 @@ routes.route('/login').post(loginUser)
 // secured
 routes.route('/logout').post(verifyToken, logoutUser)
 routes.route('/refreshToken').post(refreshAccessToken)
+routes.route('/change-password').post(verifyToken, changePassword)
+routes.route('/current-user').get(verifyToken, getCurrentUser)
+routes.route('/update-user').patch(verifyToken, updateUser)
+routes.route('/avatar').post(verifyToken, upload.single('avatar'), updateAvatar)
+routes.route('/coverImage').post(verifyToken, upload.single('coverImage'),updateCoverImage)
+routes.route('/c/:username').get(verifyToken, getUserChannelProfile)
+routes.route('/watchHistory').get(verifyToken, getWatchHistory)
 
 export default routes
